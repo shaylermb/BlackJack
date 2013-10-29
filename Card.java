f4c583125750137c067361370c2867eb5bb6cf2e
apcs <apcs@mvhs-u528ap04.dcsdk12.org>
2013-10-29 09:07:34 -0600

    Homework

---
+++ Card.java
@@ -0,0 +1,14 @@
+public class Card{
+	private int number;
+	private int value;
+
+	public Card(int value, String name){
+		this.value = value;
+		this.name = name;
+	}
+	
+
+
+
+
+}
---
+++ Deck.java
@@ -0,0 +1,15 @@
+public class Deck{
+	private Card[] cards;
+
+	public Deck(){
+		for (int i = 0; i<52; i++) {
+			Card card = new Card(1,1){
+			}
+		}
+	}
+
+
+
+
+
+}
